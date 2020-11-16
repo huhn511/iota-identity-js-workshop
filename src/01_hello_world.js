33e59248f7e3a407edb67b520a19ec10010d103c
huhn511 <sehe89@gmail.com>
2020-11-16 22:35:46 +0100

    add proper hello world example

--- src/01_hello_world.js
+++ src/01_hello_world.js
@@ -1,3 +1,4 @@
 const identity = require('iota-identity-wasm-test/node')
 
-console.log("identity", identity);
+// Prints "Hello world" if identity is correct loaded
+console.log((typeof identity == "object") ? "Hello world!" : "Error: WASM Module not loaded correctly." );
